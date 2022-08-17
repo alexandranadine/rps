54edb546eaacf1f4d9e5812d3c8245336a1c5c7f
Alexandra Lewis <alexandra.nadine.lewis@gmail.com>
2022-08-17 14:23:59 -0700

    got playGame function to work (it was running duplicates)

--- app.js
+++ app.js
@@ -3,42 +3,94 @@
 //paper beats rock
 //rock beats scissors
 
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
-
 function getComputerChoice() {
-  let rpsList = ["rock", "paper", "scissors"];
-  let random = rpsList[~~(Math.random() * rpsList.length)];
+  const rockPaperScissors = ["rock", "paper", "scissors"];
+  const random =
+    rockPaperScissors[~~(Math.random() * rockPaperScissors.length)];
   return random;
 }
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     console.log("Tie game!!");
+    console.log(
+      "the computer picked: " +
+        computerSelection +
+        "\n" +
+        "the player picked: " +
+        playerSelection +
+        "\n" +
+        "the computer's score: " +
+        computerScore +
+        "\n" +
+        "the player's score: " +
+        playerScore
+    );
   }
 
   if (
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper") ||
-    (computerSelection == "paper" && playerSelection == "rock")
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "rock")
   ) {
-    console.log("You lose");
+    computerScore++;
+    console.log("You lose! Big fat loser!");
+    console.log(
+      "the computer picked: " +
+        computerSelection +
+        "\n" +
+        "the player picked: " +
+        playerSelection +
+        "\n" +
+        "the computer's score: " +
+        computerScore +
+        "\n" +
+        "the player's score: " +
+        playerScore
+    );
   }
 
   if (
-    (computerSelection == "scissors" && playerSelection == "rock") ||
-    (computerSelection == "paper" && playerSelection == "scissors") ||
-    (computerSelection == "rock" && playerSelection == "paper")
+    (computerSelection === "scissors" && playerSelection === "rock") ||
+    (computerSelection === "paper" && playerSelection === "scissors") ||
+    (computerSelection === "rock" && playerSelection === "paper")
   ) {
+    playerScore++;
     console.log("You WIN!");
+    console.log(
+      "the computer picked: " +
+        computerSelection +
+        "\n" +
+        "the player picked: " +
+        playerSelection +
+        "\n" +
+        "the computer's score: " +
+        computerScore +
+        "\n" +
+        "the player's score: " +
+        playerScore
+    );
   }
 }
 
-console.log(
-  "the computer picked: " +
-    computerSelection +
-    "\n" +
-    "the player picked: " +
-    playerSelection
-);
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = "paper";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+  }
+}
+
+// console.log(
+//   "the computer picked: " +
+//     computerSelection +
+//     "\n" +
+//     "the player picked: " +
+//     playerSelection
+// );
+// playRound(playerSelection, computerSelection);
+console.log(playGame());
+// console.log("CS:   " + computerSelection);
